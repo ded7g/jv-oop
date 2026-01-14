19074cd3bb4f8849d90f41e30a120cc4e9a57de3
mc704ed <mykyta.churylov@student.tuke.sk>
2026-01-14 10:57:15 +0100

    update

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = new Machine[2];
+        machines[0] = new Buldozer();
+        machines[1] = new Truck();
+        machines[2] = new Excavator();
 
+        for (Machine m : machines) {
+            m.doWork();
+            m.stopWork();
+        }
+    }
 }
